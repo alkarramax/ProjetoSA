5046adb0a3e52aa2e79cd6d294867911715c1e0e
Aluno <alkarramax3@gmail.com>
2019-06-17 16:54:45 -0300

    Home_Imagens_Detalhes

--- app/src/main/java/com/example/alkar/projetosa/Home_Imagens_Detalhe.java
+++ app/src/main/java/com/example/alkar/projetosa/Home_Imagens_Detalhe.java
@@ -13,19 +13,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.alkar.projetosa.Firebase.Doacao;
 import com.example.alkar.projetosa.Firebase.Entidade;
 import com.example.alkar.projetosa.Firebase.Softplayer;
-import com.example.alkar.projetosa.Fragmentos.ProfileFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -36,8 +30,6 @@ import com.xwray.groupie.ViewHolder;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.annotation.Nullable;
 
 import static android.graphics.BitmapFactory.decodeStream;
 
@@ -67,6 +59,10 @@ public class Home_Imagens_Detalhe extends AppCompatActivity {
         Intent intent = getIntent();
         String nomeEntidade = intent.getStringExtra("nome");
 
+        CollectionReference collectionReference1 = db.collection("entidade").document(nomeEntidade)
+                .collection("Doações");
+
+
         Query queryEntidade = collectionReference.whereEqualTo("nome", nomeEntidade);
         queryEntidade.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
@@ -82,6 +78,8 @@ public class Home_Imagens_Detalhe extends AppCompatActivity {
             }
         });
 
+
+
     }
 
     public class EntidadeItem extends Item<ViewHolder> {
@@ -95,17 +93,21 @@ public class Home_Imagens_Detalhe extends AppCompatActivity {
 
         @Override
         public void bind(@NonNull ViewHolder viewHolder, int position) {
+
             final TextView nomeEntidade = viewHolder.itemView.findViewById(R.id.txtTitle);
             TextView descricao = viewHolder.itemView.findViewById(R.id.txtDescri);
             TextView descricaoObj = viewHolder.itemView.findViewById(R.id.txtObj);
             TextView data = viewHolder.itemView.findViewById(R.id.txtData);
             TextView local = viewHolder.itemView.findViewById(R.id.textLocal);
 
-            final ImageView softConfirm = viewHolder.itemView.findViewById(R.id.softDoacao);
             ImageView imageView = viewHolder.itemView.findViewById(R.id.bookthumbnail);
+            ImageView softConfirm = viewHolder.itemView.findViewById(R.id.softDoacao);
+            ImageView softConfirm2 = viewHolder.itemView.findViewById(R.id.softDoacao2);
+
             Button imageButtonDoar = viewHolder.itemView.findViewById(R.id.imageButtonDoar);
 
             CardView perfilSoft = viewHolder.itemView.findViewById(R.id.perfilSoftplayer);
+            CardView perfilSoft2 = viewHolder.itemView.findViewById(R.id.perfilSoftplayer2);
 
             nomeEntidade.setText(entidade.getNome());
             descricao.setText(entidade.getDescricao());
@@ -113,6 +115,7 @@ public class Home_Imagens_Detalhe extends AppCompatActivity {
             descricaoObj.setText(entidade.getObjetivo());
             data.setText(entidade.getData());
             local.setText(entidade.getLocal());
+
 
             imageButtonDoar.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -124,17 +127,13 @@ public class Home_Imagens_Detalhe extends AppCompatActivity {
                     final CollectionReference collectionReference = db.collection("softplayers");
                     final CollectionReference collectionReference1 = db.collection("entidade");
 
-
                     Query querySoftplayers = collectionReference.whereEqualTo("uuid", FirebaseAuth.getInstance().getUid());
-
                     querySoftplayers.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
                             if(task.isSuccessful()) {
                                 for(QueryDocumentSnapshot document : task.getResult()) {
                                     Softplayer softplayer   = document.toObject(Softplayer.class);
-                                    Entidade    entidade    = document.toObject(Entidade.class);
-
 
                                     int contador = softplayer.getContador();
                                     contador += 1;
@@ -150,7 +149,6 @@ public class Home_Imagens_Detalhe extends AppCompatActivity {
                                                     Toast.makeText(Home_Imagens_Detalhe.this, "Doação feita", Toast.LENGTH_SHORT).show();
                                                 }
                                             });
-                                    Picasso.get().load(softplayer.getProfileUrl()).into(softConfirm);
 
                                     String uuid = FirebaseAuth.getInstance().getUid();
                                     String nome = softplayer.getNome();
@@ -160,18 +158,17 @@ public class Home_Imagens_Detalhe extends AppCompatActivity {
 
                                     collectionReference1.document(nomeEntidade).collection("Doações")
                                             .document(nomeEntidade).set(softplayerDoacao);
+
                                 }
                             } else {
                                 Toast.makeText(getApplicationContext(), "Something is wrong", Toast.LENGTH_SHORT).show();
                             }
-
-
                         }
                     });
                 }
             });
 
-
+            /*
             perfilSoft.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -180,6 +177,7 @@ public class Home_Imagens_Detalhe extends AppCompatActivity {
                     startActivity(intent);
                 }
             });
+            */
         }
 
         @Override
@@ -187,11 +185,5 @@ public class Home_Imagens_Detalhe extends AppCompatActivity {
             return R.layout.cardview_home_detalhes;
         }
 
-
-    }
-
-    public void voltarTela(View view) {
-        Intent intent = new Intent(this, Home.class);
-        startActivity(intent);
     }
 }
