f57c01fc302b33015a2f207eb66c8ed456898dd7
alkarramax <alkarramax3@gmail.com>
2019-06-21 20:56:01 -0300

    teste

--- app/src/main/java/com/example/alkar/projetosa/TelaAdmin/UpdateEntidade.java
+++ app/src/main/java/com/example/alkar/projetosa/TelaAdmin/UpdateEntidade.java
@@ -2,10 +2,13 @@ package com.example.alkar.projetosa.TelaAdmin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.alkar.projetosa.R;
 
 public class UpdateEntidade extends AppCompatActivity {
+
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
