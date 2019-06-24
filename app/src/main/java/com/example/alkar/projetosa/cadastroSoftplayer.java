b44f7a92b495c98ce53f6174b44feb4036c4bd3e
amandafariassena <amandafariasssena@gmail.com>
2019-06-24 14:50:50 -0300

    :}

--- app/src/main/java/com/example/alkar/projetosa/cadastroSoftplayer.java
+++ app/src/main/java/com/example/alkar/projetosa/cadastroSoftplayer.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -62,6 +63,12 @@ public class cadastroSoftplayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro__softplayer);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
 
         textInputNome = findViewById(R.id.textSoft_input_nome);
         textInputSobrenome = findViewById(R.id.textSoft_input_sobrenome);
